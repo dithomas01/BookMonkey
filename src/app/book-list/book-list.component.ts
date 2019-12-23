43690de030a62ffa2e26378a80391d2516e2c911
Dimitri Thomas <dimitri.thomas@web.de>
2019-12-23 15:27:10 +0100

    8.1 Dependency Injection: Code in Services auslagern

--- src/app/book-list/book-list.component.ts
+++ src/app/book-list/book-list.component.ts
@@ -1,5 +1,6 @@
 import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 import {Book} from '../shared/book';
+import {BookStoreService} from '../shared/book-store.service';
 
 @Component({
   selector: 'bm-book-list',
@@ -10,38 +11,11 @@ export class BookListComponent implements OnInit {
   books: Book[];
   @Output() showDetailsEvent = new EventEmitter<Book>();
 
-  constructor() {
+  constructor(private bs: BookStoreService) {
   }
 
   ngOnInit() {
-    this.books = [
-      {
-        isbn: '9783864906466',
-        title: 'Angular',
-        authors: ['Ferdinand Malcher', 'Johannes Hoppe', 'Danny Koppenhagen'],
-        published: new Date(2019, 4, 30),
-        subtitle: 'Grundlagen, fortgeschrittene Themen und Best Practices - mit NativeScript und NgRx',
-        rating: 5,
-        thumbnails: [{
-          url: 'https://ng-buch.de/buch1.jpg',
-          title: 'Buchcover'
-        }],
-        description: 'Die Autoren führen Sie mit einem anspruchsvollen Beispielprojekt durch die Welt von Angular...'
-      },
-      {
-        isbn: '9783864903274',
-        title: 'Reacht',
-        authors: ['Oliver Zeigermann', 'Nils Hartmann'],
-        published: new Date(2016, 6, 17),
-        subtitle: 'Die praktische Einführung in React, React router und Redux',
-        rating: 3,
-        thumbnails: [{
-          url: 'https://ng-buch.de/buch2.jpg',
-          title: 'Buchcover'
-        }],
-        description: 'React ist ein JavaScript-Framework zur Entwicklung von Benutzeroberflächen...'
-      }
-    ];
+    this.books = this.bs.getAll();
   }
 
   showDetails(book: Book) {
