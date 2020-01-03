c40bab8132f073b8bca6fd5608aa605ca756fecc
Dimitri Thomas <dimitri.thomas@web.de>
2020-01-03 11:12:07 +0100

    10.2.10 Daten vom Server typisieren und umwandeln

--- src/app/shared/book-store.service.ts
+++ src/app/shared/book-store.service.ts
@@ -1,7 +1,10 @@
 import {Injectable} from '@angular/core';
 import {Book} from './book';
 import {HttpClient} from '@angular/common/http';
-import {Observable} from "rxjs";
+import {Observable} from 'rxjs';
+import {BookRaw} from './book-raw';
+import {BookFactory} from './book-factory';
+import {map} from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -11,42 +14,48 @@ export class BookStoreService {
   books: Book[];
 
   constructor(private http: HttpClient) {
-    this.books = [
-      {
-        isbn: '9783864906466',
-        title: 'Angular',
-        authors: ['Ferdinand Malcher', 'Johannes Hoppe', 'Danny Koppenhagen'],
-        published: new Date(2019, 4, 30),
-        subtitle: 'Grundlagen, fortgeschrittene Themen und Best Practices - mit NativeScript und NgRx',
-        rating: 5,
-        thumbnails: [{
-          url: 'https://ng-buch.de/buch1.jpg',
-          title: 'Buchcover'
-        }],
-        description: 'Die Autoren führen Sie mit einem anspruchsvollen Beispielprojekt durch die Welt von Angular...'
-      },
-      {
-        isbn: '9783864903274',
-        title: 'Reacht',
-        authors: ['Oliver Zeigermann', 'Nils Hartmann'],
-        published: new Date(2016, 6, 17),
-        subtitle: 'Die praktische Einführung in React, React router und Redux',
-        rating: 3,
-        thumbnails: [{
-          url: 'https://ng-buch.de/buch2.jpg',
-          title: 'Buchcover'
-        }],
-        description: 'React ist ein JavaScript-Framework zur Entwicklung von Benutzeroberflächen...'
-      }
-    ];
+    // this.books = [
+    //   {
+    //     isbn: '9783864906466',
+    //     title: 'Angular',
+    //     authors: ['Ferdinand Malcher', 'Johannes Hoppe', 'Danny Koppenhagen'],
+    //     published: new Date(2019, 4, 30),
+    //     subtitle: 'Grundlagen, fortgeschrittene Themen und Best Practices - mit NativeScript und NgRx',
+    //     rating: 5,
+    //     thumbnails: [{
+    //       url: 'https://ng-buch.de/buch1.jpg',
+    //       title: 'Buchcover'
+    //     }],
+    //     description: 'Die Autoren führen Sie mit einem anspruchsvollen Beispielprojekt durch die Welt von Angular...'
+    //   },
+    //   {
+    //     isbn: '9783864903274',
+    //     title: 'Reacht',
+    //     authors: ['Oliver Zeigermann', 'Nils Hartmann'],
+    //     published: new Date(2016, 6, 17),
+    //     subtitle: 'Die praktische Einführung in React, React router und Redux',
+    //     rating: 3,
+    //     thumbnails: [{
+    //       url: 'https://ng-buch.de/buch2.jpg',
+    //       title: 'Buchcover'
+    //     }],
+    //     description: 'React ist ein JavaScript-Framework zur Entwicklung von Benutzeroberflächen...'
+    //   }
+    // ];
   }
 
   getAll(): Observable<Book[]> {
-    return this.http.get<any[]>(`${this.api}/books`);
+    return this.http
+      .get<BookRaw[]>(`${this.api}/books`)
+      .pipe(map(booksRaw => booksRaw.map(b => BookFactory.fromRaw(b)),
+        )
+      );
   }
 
   getSingle(isbn: string): Observable<Book> {
-    return this.http.get<any>(`${this.api}/book/${isbn}`);
+    return this.http
+      .get<BookRaw>(`${this.api}/book/${isbn}`)
+      .pipe(map(b => BookFactory.fromRaw(b)));
   }
 
   remove(isbn: string): Observable<any> {
