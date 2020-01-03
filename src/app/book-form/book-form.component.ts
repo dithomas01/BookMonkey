f34bab4be4a9cd08cfc0ff8b7de20a20601b588d
dithomas01 <dimitri.thomas@web.de>
2020-01-03 16:02:23 +0100

    12.2 Template-Driven Forms: Das Formular-Template anlegen

--- src/app/book-form/book-form.component.ts
+++ src/app/book-form/book-form.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {BookFactory} from '../shared/book-factory';
 
 @Component({
   selector: 'bm-book-form',
@@ -7,7 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BookFormComponent implements OnInit {
 
-  constructor() { }
+  book = BookFactory.empty();
+
+  constructor() {
+  }
 
   ngOnInit() {
   }
--- src/app/shared/book-factory.ts
+++ src/app/shared/book-factory.ts
@@ -2,6 +2,21 @@ import {BookRaw} from './book-raw';
 import {Book} from './book';
 
 export class BookFactory {
+  static empty(): Book {
+    return {
+      isbn: '',
+      title: '',
+      authors: [''],
+      published: new Date(),
+      subtitle: '',
+      rating: 0,
+      thumbnails: [
+        {url: '', title: ''}
+      ],
+      description: ''
+    };
+  }
+
   static fromRaw(b: BookRaw): Book {
     return {
       ...b,
