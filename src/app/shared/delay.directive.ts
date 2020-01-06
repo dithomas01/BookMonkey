d66e1447919de7e16477e178c1c413ecf468cba9
dithomas01 <dimitrithomas@web.de>
2020-01-06 15:40:59 +0100

    13.2 Direktiven: das Vokabular von HTML erweitern

---
+++ src/app/shared/delay.directive.ts
@@ -0,0 +1,19 @@
+import {Directive, Input, OnInit, TemplateRef, ViewContainerRef} from '@angular/core';
+
+@Directive({
+  selector: '[bmDelay]'
+})
+export class DelayDirective implements OnInit {
+
+  @Input() bmDelay;
+
+  constructor(private templateRef: TemplateRef<any>, private viewContainerRef: ViewContainerRef) {
+  }
+
+  ngOnInit() {
+    setTimeout(() => {
+      this.viewContainerRef.createEmbeddedView(this.templateRef);
+    }, this.bmDelay);
+  }
+
+}
