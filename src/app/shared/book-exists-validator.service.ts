17883d5eb33e0d89eb696b40a7e6523190384b19
dithomas01 <dimitri.thomas@web.de>
2020-01-06 13:08:10 +0100

    12.4 Eigene Validatoren entwickeln: Der asynchrone Validator BookExistsValidatorService

--- src/app/shared/book-exists-validator.service.ts
+++ src/app/shared/book-exists-validator.service.ts
@@ -1,9 +1,23 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {BookStoreService} from './book-store.service';
+import {FormControl, ValidationErrors} from '@angular/forms';
+import {Observable, of} from 'rxjs';
+import {catchError, map} from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BookExistsValidatorService {
 
-  constructor() { }
+  constructor(private bs: BookStoreService) {
+  }
+
+  validate(control: FormControl): Observable<ValidationErrors | null> {
+    return this.bs.check(control.value).pipe(
+      map(exists => (exists === false) ? null : {
+        isbnExists: {valid: false}
+      }),
+      catchError(() => of(null))
+    );
+  }
 }
