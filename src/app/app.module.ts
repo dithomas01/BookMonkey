67aa341facf313839475a931b050dfe28f7f696e
dithomas01 <dimitrithomas@web.de>
2020-01-07 09:10:27 +0100

    14.1 Das Modulkonzept: Verweise anpassen

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,47 +3,23 @@ import {LOCALE_ID, NgModule} from '@angular/core';
 
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
-import {BookListComponent} from './books/book-list/book-list.component';
-import {BookListItemComponent} from './books/book-list-item/book-list-item.component';
-import {BookDetailsComponent} from './books/book-details/book-details.component';
 import {HomeComponent} from './admin/home/home.component';
 import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
 import {SearchComponent} from './search/search.component';
 import {TokenInterceptor} from './shared/token-interceptor';
-import {ReactiveFormsModule} from '@angular/forms';
-import {DateValueAccessorModule} from 'angular-date-value-accessor';
-import {BookFormComponent} from './admin/book-form/book-form.component';
-import {CreateBookComponent} from './admin/create-book/create-book.component';
-import {FormMessagesComponent} from './form-messages/form-messages.component';
-import {EditBookComponent} from './admin/edit-book/edit-book.component';
 import {registerLocaleData} from '@angular/common';
 import localeDe from '@angular/common/locales/de';
-import { IsbnPipe } from './books/shared/isbn.pipe';
-import { ZoomDirective } from './books/shared/zoom.directive';
-import { DelayDirective } from './books/shared/delay.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BookListComponent,
-    BookListItemComponent,
-    BookDetailsComponent,
     HomeComponent,
-    SearchComponent,
-    BookFormComponent,
-    CreateBookComponent,
-    FormMessagesComponent,
-    EditBookComponent,
-    IsbnPipe,
-    ZoomDirective,
-    DelayDirective
+    SearchComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule,
-    ReactiveFormsModule,
-    DateValueAccessorModule
+    HttpClientModule
   ],
   providers: [
     {
